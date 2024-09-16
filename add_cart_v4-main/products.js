5f2f2cabff86e157c740b9516e8197559816e228
oussama <ossamahattan@gmail.com>
2024-09-16 12:46:25 +0100

    Add settings for live server port

---
+++ add_cart_v4-main/products.js
@@ -0,0 +1,91 @@
+const products = [
+    {
+        id: '1',
+        name: 'Hyundai i20',
+        price: '200',
+        description: 'The Hyundai i20 is a sleek and modern hatchback, perfect for city driving and long trips alike. Known for its fuel efficiency and advanced features, it comes with a comfortable interior, smart infotainment system, and a spacious boot. The i20 offers multiple engine options, making it a versatile choice for any driver.',
+        model: 'i20',
+        transmission: 'Automatic',
+        interior: 'Fabric',
+        brand: 'Hyundai',
+        image: 'images/1.png',
+    },
+    {
+        id: '2',
+        name: 'Skoda Rapid',
+        price: '250',
+        description: 'The Skoda Rapid is a compact sedan that blends elegance with performance. Equipped with a powerful engine, it provides a smooth and enjoyable ride. Its spacious cabin, high-end infotainment system, and numerous safety features make it a reliable family car. Its also known for excellent mileage and long-lasting durability.',
+        model: 'Rapid',
+        transmission: 'Automatic',
+        interior: 'Fabric',
+        brand: 'Skoda',
+        image: 'images/2.png'
+    },
+    {
+        id: '3',
+        name: 'Tata Nexon',
+        price: '290',
+        description: 'Tata Nexon is a stylish subcompact SUV with bold design elements and a robust structure. Offering a comfortable ride, its packed with safety features like dual airbags and ABS. The Nexon is available in petrol and diesel variants, delivering impressive mileage and performance on both city streets and rugged terrains.',
+        model: 'Nexon',
+        transmission: 'Automatic',
+        interior: 'Fabric',
+        brand: 'Tata',
+        image: 'images/3.png'
+    },
+    {
+        id: '4',
+        name: 'Scorpio',
+        price: '200',
+        description: 'The Mahindra Scorpio is a rugged SUV designed for tough road conditions and off-road adventures. With its bold exterior, powerful engine, and high ground clearance, the Scorpio provides excellent performance in both urban and rural settings. It’s known for durability, safety features, and a spacious interior.',
+        model: 'Scorpio',
+        transmission: 'Automatic',
+        interior: 'Fabric',
+        brand: 'Mahindra',
+        image: 'images/4.png'
+    },
+    {
+        id: '5',
+        name: 'Tata Hexa',
+        price: '300',
+        description: 'Tata Hexa is a premium 7-seater SUV that offers a blend of luxury and power. It comes equipped with advanced technology, a robust engine, and all-terrain capability. With its spacious interiors, comfortable seating, and impressive safety features, the Hexa is perfect for long family trips and off-road journeys.',
+        model: 'Hexa',
+        transmission: 'Automatic',
+        interior: 'Leather',
+        brand: 'Tata',
+        image: 'images/5.png'
+    },
+    {
+        id: '6',
+        name: 'Tata Tiago',
+        price: '200',
+        description: 'The Tata Tiago is an affordable, fuel-efficient hatchback that stands out with its modern design and advanced features. It offers a smooth driving experience with comfortable seating and easy maneuverability, making it ideal for city driving. The Tiago also boasts excellent safety features, including dual airbags and ABS.',
+        model: 'Tiago',
+        transmission: 'Automatic',
+        interior: 'Fabric',
+        brand: 'Tata',
+        image: 'images/6.png'
+    },
+    {
+        id: '7',
+        name: 'Suzuki Baleno',
+        price: '220',
+        description: 'The Suzuki Baleno is a premium hatchback known for its stylish design, spacious interiors, and fuel efficiency. It’s equipped with the latest technology, including a touchscreen infotainment system and multiple safety features. The Baleno offers a smooth, comfortable ride, making it perfect for both city and highway driving.',
+        model: 'Baleno',
+        transmission: 'Automatic',
+        interior: 'Fabric',
+        brand: 'Suzuki',
+        image: 'images/7.png'
+    },
+    {
+        id: '8',
+        name: 'HYUNDAI City',
+        price: '280',
+        description: 'Hyundai City, known for its premium look and reliable performance, is a popular sedan choice. It offers a refined driving experience with advanced infotainment, safety features like airbags, and efficient fuel economy. Its spacious interior and comfortable seating make it an ideal car for families and long commutes.',
+        model: 'City',
+        transmission: 'Automatic',
+        interior: 'Fabric',
+        brand: 'Hyundai',
+        image: 'images/8.png'
+    }
+];
+export default products;
