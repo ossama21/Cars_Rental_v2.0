444779197cc0a6120816cd92c3b0c6c18b19c0d7
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2024-09-18 23:48:04 +0100

    finally i fixed the bug

--- Car-Rent-Website/data/connect.php
+++ Car-Rent-Website/data/connect.php
@@ -3,7 +3,7 @@
 $host="localhost";
 $user="root";
 $pass="";
-$db="login";
+$db="car_rent";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
